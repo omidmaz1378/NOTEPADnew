b3dff72ecbcfd2bd17150ff87dd525672518af5a
omidak1378 <omid.maz1378@gmail.com>
2021-06-19 02:52:16 +0430

    version 1.0.0 changes : // 1- basic form // 2- new button basic code

---
+++ NOTEPAD/frmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NOTEPAD
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = ProductVersion.ToString();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            //basic code
+            richTextBox1.Clear();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //basic code
+            richTextBox1.Clear();   
+        }
+    }
+}
